692b08057bcfb1e9571375ba88f838519872d031
Mohammed Metawea <mohammed.metawea@nike.com>
2024-04-24 13:29:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# minigrep
+![Rust-build](https://github.com/mgabs/minigrep/workflows/Rust-build/badge.svg)
+
+A sample implementation of minigrep.
+Exercise for actions & CI
+
---
+++ experiments/claps.rs
@@ -0,0 +1,64 @@
+// (Full example with detailed comments in examples/01a_quick_example.rs)
+//
+// This example demonstrates clap's "builder pattern" method of creating arguments
+// which the most flexible, but also most verbose.
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("My Super Program")
+        .version("1.0")
+        .author("Kevin K. <kbknapp@gmail.com>")
+        .about("Does awesome things")
+        .arg(
+            Arg::new("config")
+                .short('c')
+                .long("config")
+                .value_name("FILE")
+                .about("Sets a custom config file")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::new("INPUT")
+                .about("Sets the input file to use")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::new("v")
+                .short('v')
+                .multiple(true)
+                .about("Sets the level of verbosity"),
+        )
+        .subcommand(
+            App::new("test")
+                .about("controls testing features")
+                .version("1.3")
+                .author("Someone E. <someone_else@other.com>")
+                .arg(
+                    Arg::new("debug")
+                        .short('d')
+                        .about("print debug information verbosely"),
+                ),
+        )
+        .get_matches();
+
+    // Same as above examples...
+    match opts.verbose {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    match opts.subcmd {
+        SubCommand::Test(t) => {
+            if t.debug {
+                println!("Printing debug info...");
+            } else {
+                println!("Printing normally...");
+            }
+        }
+    }
+}
